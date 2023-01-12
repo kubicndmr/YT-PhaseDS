aaa3ed2a9d9e1294bf78827d5fc029d7f036fc58
Kubi <44262506+kubicndmr@users.noreply.github.com>
2023-01-12 17:13:47 +0100

    Add files via upload

--- queries.py
+++ queries.py
@@ -1,15 +1,17 @@
-first_queries = ["pflanzen", "maschine", "kochen", "wartung", "geographie", "technologie", "AeroNewsGermany", "geschichte", "psychologie", "motorsport magazin", # fiels
-"klimawandel", "wirtschaft", "nachrichten", "krieg", "pandemie", "bau", # politic
-"studium", "universität", "bibliothek", "wissenschaft", "forschung", "geschichte", "literatur", # study
-"urlaub", "reise", "flug", "marokko", "griechenland", "terra x", "strand", # travel
-"hörbuch" , "kochen", "arbeit", "wanderung", "deutsch", "evolution", "fahrrad",]
-second_queries = ["krankheit", "krankenhaus", "notarzt", "arzt", "medizin", "uniklinik", # hospital
-" Biographer DE", "augen", "knochen", "haut", "blut", "vene", "schmerz", # body  
-"chirurgie", "implantation", "operationsraum", "chirurg", "facharzt", # surgery
-"interventionelle Radiologie", "radiologie", "krebs", "verletzungen", "gefäßerkrankungen", 
-"Nierenerkrankungen", "blutkreislauf erkrankungen"] # radiologie
-third_quaries = [ "portkatheter", "portimplantation", "portkanüle", "röntgen", "röhrchen", "schläuche", "strahlen",
-"Strahlenschutz", "betäubung", "ultraschall", # PoCaP
-"universität heidelberg", "universität stuttgart", "universität erlangen", "universität hamburg", #universities
-"universität münchen", "universität tübingen", "technische universität", "universität wien",
-"flughafen", "flugzeug", "fluglinie"] # aviation
+search_queries = ["medizin", "chirurgie", "operationsraum", "krankenhaus", "facharzt", "klinik",
+	"anatomie", "psychologie", "allgemeinmedizin", "notfallmedizin", "strahlenbehandlung",
+	"strahlenschutz", "röntgen", "epidemiologie", "medizinische informatik", "mikrobiologie", 
+	"pathologie", "pharmakologie", "unfallchirurgie", "humangenetik", "innere medizin",
+	"klinische chemie", "hämatologie", "labormedizin", "transfusionsmedizin", "hämostaseologie",
+	"anästhesiologie", "frauenheilkunde", "geburtshilfe", "pathophysiologie", "kinderheilkunde",
+	"jugendmedizin", "orthopädie", "physikalische medizin", "infektiologie", "immunologie",
+	"rechtsmedizin", "urologie", "radiologie", "augenheilkunde", "dermatologie", "venerologie",
+	"hals nasen ohren", "neurologie", "psychiatrie", "psychosomatik", "palliativmedizin",
+	"schmerzmedizin", "kardiologie", "onkologie", "bildgebungsverfahren in der radiologie", 
+	"interventionelle radiologie", "innere blutungen", "gefäßchirurgie", "endovaskuläre chirurgie", 
+	"computertomographie", "magnet resonanz tomographie", "kernspintomographie", "ultraschall",
+	"gefäßverschlüsse", "blutungen", "tumoren", "lebersystem", "gallengangsystem", "aortenruptur",
+	"aortenaneurysma", "aortendissektion", "darmtrakts", "beckenbodens", "magentrakts", "sonographie",
+	"röntgenuntersuchung", "katheter", "röhrchen", "schlauch", "portkatheter", "port-katheter",  
+	"portkanüle", "digitale subtraktionsangiographie", "durchleuchtung", "angiographie", 
+	"portkathetersystem", "betäubung", "blutkreislauf", "infusion", "gallenwege"]
